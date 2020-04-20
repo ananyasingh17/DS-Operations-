2e8d102d9afc68b4c3a0d6144b10e9d5d9f12a13
ananyasingh17 <60870489+ananyasingh17@users.noreply.github.com>
2020-04-20 10:54:24 +0530

    Add files via upload

---
+++ BinaryTreeNode.java
@@ -0,0 +1,20 @@
+package com.tree;
+
+public class BinaryTreeNode {
+
+	// Java program for different tree traversals 
+
+	/* Class containing left and right child of current 
+	node and key value*/
+	
+		int key; 
+		BinaryTreeNode left, right; 
+
+		public BinaryTreeNode(int item) 
+		{ 
+			key = item; 
+			left = right = null; 
+		} 
+	}
+
+	
