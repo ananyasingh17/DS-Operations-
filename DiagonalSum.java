f4b0bec87b6e27ea12018fb5cb97d08102514e20
ananyasingh17 <60870489+ananyasingh17@users.noreply.github.com>
2020-04-24 21:58:36 +0530

    Update DiagonalSum.java

--- DiagonalSum.java
+++ DiagonalSum.java
@@ -1,38 +1,32 @@
 package array;
 
-public class DiagonalSum {
+// An efficient java program to find 
+// sum of diagonals 
+import java.io.*; 
 
-	static void printDiagonalSums(int[][] mat, int n) {
-		int principal = 0, secondary = 0;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
+public class GFG { 
 
-
-				if (i == j)
-					principal += mat[i][j];
-
- 
-				if ((i + j) == (n - 1))
-					secondary += mat[i][j];
-			}
-		}
-
-		System.out.println("Principal Diagonal:" + principal);
-
-		System.out.println("Secondary Diagonal:" + secondary);
-	}
-
-
-static public void main (String[] args) 
-{ 
-
-int [][]a = { { 9, 2, 7, 1 }, 
-{ 1, 6, 9, 8 },  
-{ 9, 5, 3, 9 }, 
-{ 1, 9, 3, 2 } }; 
-
-printDiagonalSums(a, 4); 
+	static void printDiagonalSums(int [][]mat,int n) 
+	{ 
+		int principal = 0, secondary = 0; 
+		for (int i = 0; i < n; i++) { 
+			principal += mat[i][i]; 
+			secondary += mat[i][n - i - 1]; 
+		} 
+	
+		System.out.println("Principal Diagonal:"+ principal); 
+									
+		System.out.println("Secondary Diagonal:"+ secondary); 
+	} 
+	
+	// Driver code 
+	static public void main (String[] args) 
+	{ 
+		int [][]a = { { 1, 2, 3, 4 }, 
+					{ 5, 6, 7, 8 }, 
+					{ 1, 2, 3, 4 }, 
+					{ 5, 6, 7, 8 } }; 
+	
+		printDiagonalSums(a, 4); 
+	} 
 } 
-} 
-
-
